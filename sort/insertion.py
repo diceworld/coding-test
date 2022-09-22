b1ecb58c63a75740a9c799e953f3f7b66fc1c055
diceworld <diceworld@naver.com>
2022-09-22 21:10:03 +0900

    삽입정렬

---
+++ sort/insertion.py
@@ -0,0 +1,10 @@
+array = [6, 9, 5, 1, 3, 0, 8, 2, 4, 7]
+
+for i in range(1, len(array)):
+    for j in range(i, 0, -1):
+        if array[j] < array[j - 1]:
+            array[j], array[j - 1] = array[j - 1], array[j]
+        else:
+            break
+
+print(array)
