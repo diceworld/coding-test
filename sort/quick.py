d67f2f47caec54c23a7e088f936a1c1f902b06f7
diceworld <diceworld@naver.com>
2022-09-23 20:24:27 +0900

    퀵정렬

---
+++ sort/quick.py
@@ -0,0 +1,32 @@
+array = [6, 9, 5, 1, 3, 0, 8, 2, 4, 7]
+
+
+def quick_sort(arr, start, end):
+    if start >= end:  # 원소가 1개인 경우 종료
+        return
+
+    pivot = start  # 피벗은 첫 번째 원소
+    left = start + 1
+    right = end
+
+    while left <= right:
+        # 피벗보다 큰 데이터를 찾을 때까지 반복
+        while left <= end and arr[left] <= arr[pivot]:
+            left += 1
+
+        # 피벗보다 작은 데이터를 찾을 때까지 반복
+        while right > start and arr[right] >= arr[pivot]:
+            right -= 1
+
+        if left > right:  # 엇갈렸다면 작은 데이터와 피벗을 교체
+            arr[right], arr[pivot] = arr[pivot], arr[right]
+        else:  # 엇갈리지 않았다면 작은 데이터와 큰 데이터를 교체
+            arr[left], arr[right] = arr[right], arr[left]
+
+    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
+    quick_sort(arr, start, right - 1)
+    quick_sort(arr, right + 1, end)
+
+
+quick_sort(array, 0, len(array) - 1)
+print(array)
