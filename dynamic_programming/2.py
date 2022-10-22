e6a19e35b627ee65eb9fb658ed90292e2c2d3fc6
diceworld <diceworld@naver.com>
2022-10-22 21:05:20 +0900

    dynamic programming 2번 문제

---
+++ dynamic_programming/2.py
@@ -0,0 +1,15 @@
+"""
+dynamic programming 2
+"""
+n = int(input())  # N
+array = list(map(int, input().split()))  # 식량 창고
+
+d = [0] * n
+
+d[0] = array[0]
+d[1] = max(array[0], array[1])
+
+for i in range(2, n):
+    d[i] = max(d[i - 1], d[i - 2] + array[i])
+
+print(d[n - 1])
